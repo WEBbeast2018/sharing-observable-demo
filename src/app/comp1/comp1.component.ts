a6e1d23e52e8949b9d62e733761877b6d573f05f
yuvalbl <yuval.bar.levi@gmail.com>
2018-08-08 17:49:35 +0300

    unsubscription demo

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DummyService } from '../dummy.service';
+import { map } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-comp1',
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.css']
 })
-export class Comp1Component implements OnInit, OnDestroy {
+export class Comp1Component implements OnInit {
   data;
-  subscription;
   constructor(private dummyService: DummyService) { }
 
   ngOnInit() {
-    this.subscription = this.dummyService.getData().subscribe(d => {
-      this.data = d;
-    });
+    this.data = this.dummyService.getData().pipe(
+      map(x => x + '  some suffix '),
+      map(x => x + '  :)')
+    );
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 }
